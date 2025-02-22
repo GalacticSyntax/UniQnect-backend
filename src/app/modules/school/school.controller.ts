69eb58355372ad7654da114fa42a968ddd9dabe7
developerHub01 <developerhubytchannel@gmail.com>
2025-02-23 02:24:38 +0600

    refactor: streamline school creation response and enhance error handling

--- src/app/modules/school/school.controller.ts
+++ src/app/modules/school/school.controller.ts
@@ -1,19 +1,20 @@
 import { Request, Response } from "express";
 import { SchoolModel } from "./model/model";
 import catchAsync from "../../utils/catch.async";
-
+import { sendResponse } from "../../utils/send.response";
+import httpStatus from "http-status";
 
 export const createSchool = catchAsync(async (req: Request, res: Response) => {
-  try {
-    const school = await SchoolModel.create(req.body);
-    res.status(201).json({ success: true, data: school });
-  } catch (error) {
-    const errMessage =
-      error instanceof Error ? error.message : "An unknown error occurred";
-    res.status(400).json({ success: false, message: errMessage });
-  }
-});
+  const school = await SchoolModel.create(req.body);
+  res.status(201).json({ success: true, data: school });
 
+  return sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: "School created successfully",
+    data: school,
+  });
+});
 
 export const updateSchool = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -40,17 +41,19 @@ export const updateSchool = catchAsync(async (req: Request, res: Response) => {
   res.json({ success: true, data: school });
 });
 
+export const getAllSchools = catchAsync(
+  async (_req: Request, res: Response) => {
+    const schools = await SchoolModel.find();
+    res.json({ success: true, data: schools });
+  },
+);
 
-export const getAllSchools = catchAsync(async (_req: Request, res: Response) => {
-  const schools = await SchoolModel.find();
-  res.json({ success: true, data: schools });
-});
-
-
-export const getSchoolsByQuery = catchAsync(async (req: Request, res: Response) => {
-  const query = req.query;
-  const schools = await SchoolModel.find(query);
-  res.json({ success: true, data: schools });
-});
+export const getSchoolsByQuery = catchAsync(
+  async (req: Request, res: Response) => {
+    const query = req.query;
+    const schools = await SchoolModel.find(query);
+    res.json({ success: true, data: schools });
+  },
+);
 
 // export const SchoolController = {};
