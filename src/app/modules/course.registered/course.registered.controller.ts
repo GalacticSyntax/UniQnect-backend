2687ffe94a571cb8b82729d468c9d60e128aa132
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2025-08-13 18:24:01 +0600

    comment 2

--- src/app/modules/course.registered/course.registered.controller.ts
+++ src/app/modules/course.registered/course.registered.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { courseRegistrationValidation } from "./course.registered.validation";
 import { CourseRegisteredService } from "./course.registered.service";
+import { StudentModel } from "../student/model/model";
+import { CourseRegisteredModel } from "./model/model";
 
 const registerCourseController = async (req: Request, res: Response) => {
   try {
@@ -18,6 +20,32 @@ const getCourseRegistrationsController = async (_req: Request, res: Response) =>
     res.status(200).json({ success: true, data: registrations });
   } catch (err: any) {
     res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+const myRegisteredCourses = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params; 
+
+    const student = await StudentModel.findOne({ userId });
+
+    if (!student) {
+      return res.status(400).json({ message: "studentId is required" });
+    }
+
+    const registeredCourses = await CourseRegisteredModel.find({ studentId: student?.studentId })
+      .populate("courseList");
+
+
+    res.status(200).json({
+      message: "Registered courses fetched successfully",
+      data: registeredCourses,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching registered courses",
+      error: error instanceof Error ? error.message : error,
+    });
   }
 };
 
